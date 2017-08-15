5d2f3f16944ebd40381bc5d1d95c4c3af2fa29b8
ulc5410 <ulc5410@gmail.com>
2017-08-15 16:42:43 +0800

    index changes
    +demo ,slideshow auto

--- assets/js/slideshow.js
+++ assets/js/slideshow.js
@@ -1,6 +1,6 @@
-var slideIndex = 1;
+var slideIndex = 0;
 showSlides(slideIndex);
-
+carousel();
 function plusSlides(n) {
   showSlides(slideIndex += n);
 }
@@ -24,3 +24,15 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
 } 
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > x.length) {slideIndex = 1}
+    x[slideIndex-1].style.display = "block";
+    setTimeout(carousel, 4000); // Change image every 2 seconds
+}
+
