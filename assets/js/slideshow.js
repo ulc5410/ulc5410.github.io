98a4b25d962746ae2fd570e5c7b2ff27ee553287
ulc5410 <ulc5410@gmail.com>
2017-08-16 11:09:51 +0800

    slidshow button + NTU stuff

--- assets/js/slideshow.js
+++ assets/js/slideshow.js
@@ -1,6 +1,6 @@
-var slideIndex = 0;
-showSlides(slideIndex);
+var slideIndex = 3;
 carousel();
+
 function plusSlides(n) {
   showSlides(slideIndex += n);
 }
@@ -12,6 +12,7 @@ function currentSlide(n) {
 function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
+  var y = document.getElementsByClassName("slides_btn");
   var dots = document.getElementsByClassName("dot");
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
@@ -23,16 +24,33 @@ function showSlides(n) {
   }
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
+    for (i = 0; i < y.length; i++) {
+      y[i].style.display = "none";
+    }
+	if(slideIndex === 1){
+      y[0].style.display = "inline-block";
+	}else if(slideIndex === 3){
+      y[1].style.display = "inline-block";	
+	}
 } 
 function carousel() {
     var i;
     var x = document.getElementsByClassName("mySlides");
+    var y = document.getElementsByClassName("slides_btn");
     for (i = 0; i < x.length; i++) {
       x[i].style.display = "none";
     }
     slideIndex++;
     if (slideIndex > x.length) {slideIndex = 1}
     x[slideIndex-1].style.display = "block";
-    setTimeout(carousel, 4000); // Change image every 2 seconds
+    setTimeout(carousel, 4000); // Change image every 4 seconds
+    for (i = 0; i < y.length; i++) {
+      y[i].style.display = "none";
+    }
+	if(slideIndex === 1){
+      y[0].style.display = "inline-block";
+	}else if(slideIndex === 3){
+      y[1].style.display = "inline-block";	
+	}
 }
 
